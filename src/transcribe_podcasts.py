f4b110cfb28d8fe290439cabe851bb68893fe6fc
SamEdwardes <edwardes.s@gmail.com>
2020-02-19 16:39:11 -0800

    checkpoint, transcribe not working for large files

--- src/transcribe_podcasts.py
+++ src/transcribe_podcasts.py
@@ -6,42 +6,51 @@ from datetime import datetime
 
 from collections import defaultdict
 
+# read in tracker
 df = pd.read_csv("data/episode_tracker.csv")
 df["published"] = pd.to_datetime(df["published"])
 
-meta = podcast_meta = df.iloc[0:3,:]
-def download_episode(podcast_meta):
-    # name files
-    podcast_meta = df.iloc[0,:]
-    year = str(podcast_meta["published"].year)
-    month = str(podcast_meta["published"].month).zfill(2)
-    day = str(podcast_meta["published"].day).zfill(2)
-    date = year + "-" + month + "-" + day
-    out_tag = date + "__" + podcast_meta["title"].lower().replace(" ", "_")
+# add in extra metadata
+df["year"] = df["published"].apply(lambda x: str(x.year))
+df["month"] = df["published"].apply(lambda x: str(x.month).zfill(2))
+df["day"] = df["published"].apply(lambda x: str(x.day).zfill(2))
+df["date"] = df["year"] + "-" + df["month"] + "-" + df["day"]
+df["out_tag"] = df["date"] + "__" + df["title"].apply(lambda x: x.lower().replace(" ", "_"))
+df["out_tag"] = df["out_tag"].apply(lambda x: x.replace("#", ""))
 
+
+def download_episode(url, out_tag):
     # download episode
     mp3_out = "data/audio/" + out_tag + ".mp3"
-    urllib.request.urlretrieve(podcast_meta["url"], mp3_out)
-
-    # convert wav to mp3 
-    wav_out = "data/audio/" + out_tag + ".wav"                                                          
+    print("downloading: " + mp3_out)
+    urllib.request.urlretrieve(url, mp3_out)
+    # convert mp3 to wav
+    wav_out = "data/audio/" + out_tag + ".wav"
+    print("converting mp3 to wav: " + wav_out)
     mp3 = pydub.AudioSegment.from_mp3(mp3_out)
     mp3.export(wav_out, format="wav")
-
     return None
 
 
-def transcribe_episode():
-    # create transcript
+def transcribe_episode(out_tag):
+    # transcribe audio to text
     transcript_out = "data/transcripts/" + out_tag + ".txt"
     r = sr.Recognizer()
-    podcast_file = sr.AudioFile('data/audio/jre_test_clip.wav')
+    podcast_file = sr.AudioFile("data/audio/" + out_tag + ".wav")
+    print("recording audio file...")
     with podcast_file as source:
         audio = r.record(source)
+    print("transcribing audio file...")
     trans = r.recognize_google(audio)
-    print(trans)
+    # write results to disk
+    print("writing transcription to disk...")
+    text_file = open(transcript_out, "w")
+    text_file.write(trans)
+    text_file.close()
+    return trans
 
 
-
-
-
+# testing
+# download_episode(df.loc[0, "url"], df.loc[0, "out_tag"])
+# transcribe_episode("jre_test_clip")
+transcribe_episode(df.loc[0, "out_tag"])
